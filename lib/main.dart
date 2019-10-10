acb673418d29165a1738d2c4ef5a6a61018cc275
Larissa Marães <larissa.maraes@jera.com.br>
2019-10-09 21:40:47 -0400

    first quizz app

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  final questions = const [
+    {
+      'questionText': "What\'s your favorite color?",
+      'answers': ['Black', 'Red', 'Green', 'White']
+    },
+    {
+      'questionText': "What\'s your favorite animal?",
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+    },
+    {
+      'questionText': "Who\'s your favorite instructor?",
+      'answers': ['Larissa', 'Lari ;D', 'Larissa', 'Lari :)']
+    },
+  ];
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print('Answer chosen!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it!!'),
+              ),
+      ),
+    );
+  }
+}
